40983b1f39ac5194ccaf3ae8112c339f44e2e7db
Denis Maximenko <maximenko@owlcat.games>
2024-10-20 18:22:43 +0400

    Add StaticBox3D that can be used for blockout

---
+++ GodotToolbox/3D/static_box_3d.gd
@@ -0,0 +1,44 @@
+@tool
+class_name StaticBox3D
+extends StaticBody3D
+
+var _shape_res : BoxShape3D:
+	get:
+		return %Collision.shape
+
+var _mesh_res : BoxMesh:
+	get:
+		return %Mesh.mesh
+
+var _size : Vector3 = Vector3(1.0, 1.0, 1.0)
+@export_custom(PROPERTY_HINT_NONE, "suffix:m") var size : Vector3:
+	get:
+		return _size
+	set(new_size):
+		_size = new_size
+		if is_inside_tree():
+			_update_size()
+
+func _update_size() -> void:
+	_shape_res.size = size
+	_mesh_res.size = size
+
+var _material_res : StandardMaterial3D:
+	get:
+		return _mesh_res.material
+
+var _color : Color = Color.GRAY
+@export var color : Color:
+	get:
+		return _color
+	set(new_color):
+		_color = new_color
+		if is_inside_tree():
+			_update_color()
+
+func _update_color() -> void:
+	_material_res.albedo_color = _color
+
+func _ready() -> void:
+	_update_size()
+	_update_color()
